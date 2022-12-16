8b1ad3dd71a1e52fe56d67d8f496a1a97b491f8e
Rasdy <rasdy.wibowo@binus.ac.id>
2022-12-16 21:28:47 +0700

    Menit 20:00
    Kira" progres sampe tutorial menit 20

--- Assets/Scripts/Bar.cs
+++ Assets/Scripts/Bar.cs
@@ -11,6 +11,10 @@ public class Bar : MonoBehaviour
     public HingeJoint2D StartJoint;
     public HingeJoint2D EndJoint;
 
+    float startJointCurrentLoad = 0;
+    float endJointCurrentLoad = 0;
+    MaterialPropertyBlock propBlock;
+
     public void UpdateCreatingBar(Vector2 ToPosition)
     {
         transform.position = (ToPosition + StartPosition) / 2;
@@ -23,5 +27,22 @@ public class Bar : MonoBehaviour
         barSpriteRenderer.size = new Vector2(Length, barSpriteRenderer.size.y);
 
         boxCollider.size = barSpriteRenderer.size;
+    }
+
+    public void UpdateMaterial()
+    {
+        if (StartJoint) startJointCurrentLoad = StartJoint.reactionForce.magnitude / StartJoint.breakForce;
+        if (EndJoint) endJointCurrentLoad = EndJoint.reactionForce.magnitude / EndJoint.breakForce;
+        float maxLoad = Mathf.Max(startJointCurrentLoad, endJointCurrentLoad);
+
+        propBlock = new MaterialPropertyBlock();
+        barSpriteRenderer.GetPropertyBlock(propBlock);
+        propBlock.SetFloat("_Load", maxLoad);
+        barSpriteRenderer.SetPropertyBlock(propBlock);
+    }
+
+    private void Update()
+    {
+        if (Time.timeScale == 1) UpdateMaterial();
     }
 }
